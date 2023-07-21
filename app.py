176e67391179acc1f4e255dd1401f790ee314045
Russell Jones <justyrones@gmail.com>
2023-07-21 14:57:14 -0600

    fixing

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 database_url = os.environ['DATABASE_URL']
 database_url = database_url.replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
